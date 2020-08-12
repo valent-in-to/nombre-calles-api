6edf10899f458b4499996db03b47c319f7c58dde
v <valentinprimo@gmail.com>
2020-08-12 20:28:34 -0300

    corregido

--- app.py
+++ app.py
@@ -10,21 +10,24 @@ DATABASE = './database.sqlite3'
 @app.route('/localidades', methods=['POST'])
 @cross_origin()
 def xd():
-    if request.method == 'POST':
-        req = request.get_json(force=True) 
-        print(type(req))
-        response = []
+        try:
+        if request.method == 'POST':
+            req = request.get_json(force=True) 
+            print(type(req))
+            response = []
 
-        db = sqlite3.connect(DATABASE)
-        for i in req:
-            db.row_factory = sqlite3.Row
-            cur = db.cursor()
-            x = cur.execute('select centroide_lat, centroide_lon from localidades where localidad_censal_id=?',(req[i],))
-            y = dict(x.fetchone())
-            response.append(y)
-        
-        return jsonify(response)
-    return "xd"
+            db = sqlite3.connect(DATABASE)
+            for i in req:
+                db.row_factory = sqlite3.Row
+                cur = db.cursor()
+                x = cur.execute('select centroide_lat, centroide_lon from localidades where localidad_censal_id=?',(req[i],))
+                y = dict(x.fetchone())
+                response.append(y)
+            
+            return jsonify(response)
+        except:
+            pass
+    return "BAD REQUEST"
 
 
 if __name__ == "__main__":
